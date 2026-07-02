fa60943325eb7c8b44e26062889c803ee300164c
Jeshua <jettirincon64@gmail.com>
2026-07-02 11:47:34 -0600

    rey 07/02/26

--- modules/requests/presentation/components/RequestForm.tsx
+++ modules/requests/presentation/components/RequestForm.tsx
@@ -1,11 +1,15 @@
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { Picker } from "@react-native-picker/picker";
+import * as ImagePicker from "expo-image-picker";
 import { Stack, useRouter } from "expo-router";
-import { useState } from "react";
-import { Alert, Image, ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
-
+import { useEffect, useState } from "react";
+import { Alert, Dimensions, Image, ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
 import { CreateRequestUseCase } from "../../application/create";
 import { SupabaseRequestsRepository } from "../../infraestructure/requestsDatasurce";
+
+const { width } = Dimensions.get("window");
+const BANNER_HEIGHT = width * 0.42;
 
 const repository = new SupabaseRequestsRepository();
 const createRequest = new CreateRequestUseCase(repository);
@@ -18,6 +22,46 @@ export default function RequestForm() {
   const [numTipoMantenimiento, setNumTipoMantenimiento] = useState("");
   const [descripcion, setDescripcion] = useState("");
   const [numArea, setNumArea] = useState("");
+  const [nombreUsuario, setNombreUsuario] = useState("");
+  const [fecha] = useState(new Date().toISOString().split("T")[0]);
+  const [imagenes, setImagenes] = useState<string[]>([]);
+const [bannerPage, setBannerPage] = useState(0);
+
+  const seleccionarImagen = async () => {
+
+    const permiso = await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+    if (!permiso.granted) {
+      Alert.alert("Debes permitir acceso a las fotos");
+      return;
+    }
+
+    const resultado = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsMultipleSelection: true,
+      quality: 0.8,
+    });
+
+    if (!resultado.canceled) {
+
+      const nuevas = resultado.assets.map((a) => a.uri);
+
+      setImagenes([...imagenes, ...nuevas]);
+    }
+  };
+
+  useEffect(() => {
+    const cargarUsuario = async () => {
+      const userData = await AsyncStorage.getItem("user");
+
+      if (userData) {
+        const user = JSON.parse(userData);
+
+        setNombreUsuario(user.nombre);
+      }
+    };
+    cargarUsuario();
+  }, []);
 
   const enviarSolicitud = async () => {
 
@@ -67,32 +111,65 @@ export default function RequestForm() {
 
         <View style={styles.form}>
 
-          <Text style={styles.label}>Tipo de solicitud (numTipo)</Text>
+          <Text style={styles.label}>Fecha</Text>
+
           <TextInput
-            style={styles.input}
-            placeholder="1 = servicio / 2 = mantenimiento"
-            value={numTipo}
-            onChangeText={setNumTipo}
-            keyboardType="numeric"
+            style={styles.inputDisabled}
+            editable={false}
+            value={fecha}
+          />
+          <Text style={styles.label}>Solicitante</Text>
+
+          <TextInput
+            style={styles.inputDisabled}
+            editable={false}
+            value={nombreUsuario}
           />
 
-          <Text style={styles.label}>Tipo mantenimiento (opcional)</Text>
-          <TextInput
-            style={styles.input}
-            placeholder="ID tipo mantenimiento"
-            value={numTipoMantenimiento}
-            onChangeText={setNumTipoMantenimiento}
-            keyboardType="numeric"
-          />
+          <Text style={styles.label}>Tipo de solicitud</Text>
 
+          <View style={styles.pickerContainer}>
+            <Picker
+              selectedValue={numTipo}
+              onValueChange={(itemValue) => setNumTipo(itemValue)}
+            >
+              <Picker.Item label="Seleccione..." value="" />
+              <Picker.Item label="Servicio" value="1" />
+              <Picker.Item label="Mantenimiento" value="2" />
+            </Picker>
+          </View>
+
+          {numTipo === "2" && (
+            <>
+              <Text style={styles.label}>Tipo de mantenimiento</Text>
+
+              <View style={styles.pickerContainer}>
+                <Picker
+                  selectedValue={numTipoMantenimiento}
+                  onValueChange={(itemValue) => setNumTipoMantenimiento(itemValue)}
+                >
+                  <Picker.Item label="Seleccione..." value="" />
+                  <Picker.Item label="Preventivo" value="1" />
+                  <Picker.Item label="Correctivo" value="2" />
+                  <Picker.Item label="Reactivo" value="3" />
+                </Picker>
+              </View>
+            </>
+          )}
           <Text style={styles.label}>Área</Text>
-          <TextInput
-            style={styles.input}
-            placeholder="ID del área"
-            value={numArea}
-            onChangeText={setNumArea}
-            keyboardType="numeric"
-          />
+
+          <View style={styles.pickerContainer}>
+            <Picker
+              selectedValue={numArea}
+              onValueChange={(itemValue) => setNumArea(itemValue)}
+            >
+              <Picker.Item label="Seleccione un área..." value="" />
+              <Picker.Item label="Administración" value="1" />
+              <Picker.Item label="Fábrica" value="2" />
+              <Picker.Item label="Campo" value="3" />
+              <Picker.Item label="Zona habitacional" value="4" />
+            </Picker>
+          </View>
 
           <Text style={styles.label}>Descripción</Text>
           <TextInput
@@ -101,6 +178,66 @@ export default function RequestForm() {
             value={descripcion}
             onChangeText={setDescripcion}
           />
+
+          <Text style={styles.label}>Evidencias</Text>
+
+          <TouchableOpacity
+            style={styles.button}
+            onPress={seleccionarImagen}
+          >
+            <Text style={styles.buttonText}>
+              Seleccionar imágenes
+            </Text>
+          </TouchableOpacity>
+      
+      {imagenes.length > 0 && (
+  <>
+    <ScrollView
+      horizontal
+      pagingEnabled
+      showsHorizontalScrollIndicator={false}
+      onScroll={(e) =>
+        setBannerPage(
+          Math.round(e.nativeEvent.contentOffset.x / width)
+        )
+      }
+      scrollEventThrottle={16}
+    >
+      {imagenes.map((img, index) => (
+        <View
+          key={index}
+          style={{
+            width,
+            alignItems: "center",
+            marginVertical: 10,
+          }}
+        >
+          <Image
+            source={{ uri: img }}
+            style={{
+              width: width * 0.9,
+              height: BANNER_HEIGHT,
+              borderRadius: 20,
+            }}
+            resizeMode="cover"
+          />
+        </View>
+      ))}
+    </ScrollView>
+
+    <View style={styles.BP}>
+      {imagenes.map((_, i) => (
+        <View
+          key={i}
+          style={[
+            styles.dot,
+            bannerPage === i && styles.dotActive,
+          ]}
+        />
+      ))}
+    </View>
+  </>
+)}
 
           <TouchableOpacity style={styles.button} onPress={enviarSolicitud}>
             <Text style={styles.buttonText}>Enviar solicitud</Text>
@@ -204,4 +341,37 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     fontSize: 16,
   },
+  pickerContainer: {
+    borderWidth: 1,
+    borderColor: "#DDD",
+    borderRadius: 10,
+    backgroundColor: "#FFF",
+    marginBottom: 15,
+  },
+  inputDisabled: {
+    backgroundColor: "#d8d5d5",
+    borderWidth: 1,
+    borderColor: "#acacac",
+    borderRadius: 10,
+    padding: 10,
+    marginBottom: 10,
+    color: "#6B7280",
+  },
+  BP: {
+  flexDirection: "row",
+  justifyContent: "center",
+  marginTop: 8,
+  marginBottom: 15,
+},
+
+dot: {
+  width: 8,
+  height: 8,
+  borderRadius: 4,
+  backgroundColor: "#ccc",
+  margin: 5,
+},
+dotActive: {
+  backgroundColor: "#148248",
+},
 });
