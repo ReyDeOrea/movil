d53c77e8eb2ffbf553cc7c4bc09a0ce7961934e9
Jeshua <jettirincon64@gmail.com>
2026-07-08 13:03:58 -0600

    cmabios en app.json y layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,26 +1,31 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from "@react-navigation/native";
+import { DefaultTheme, ThemeProvider } from "@react-navigation/native";
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
+import * as SystemUI from "expo-system-ui";
+import { useEffect } from "react";
+import { Appearance } from "react-native";
 import "react-native-reanimated";
-
-import { useColorScheme } from "@/hooks/use-color-scheme";
 
 export const unstable_settings = {
   anchor: "(tabs)",
 };
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
+  useEffect(() => {
+    Appearance.setColorScheme("light");
+    SystemUI.setBackgroundColorAsync("#F5F5F5");
+  }, []);
 
   return (
-    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+    <ThemeProvider value={DefaultTheme}>
       <Stack>
         <Stack.Screen name="login" options={{ headerShown: false }} />
         <Stack.Screen name="requests" options={{ headerShown: false }} />
         <Stack.Screen name="createRequest" options={{ headerShown: false }} />
         <Stack.Screen name="viewRequestForm" options={{ headerShown: false }} />
       </Stack>
-      <StatusBar style="auto" />
+
+      <StatusBar style="light" backgroundColor="#148248" />
     </ThemeProvider>
   );
 }
